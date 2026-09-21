a612477d6d2ec6664a04a65f84728634b88b2032
elymouthon <ely.mouthon@outlook.fr>
2026-09-21 14:49:21 +0000

    refactor(frontend): ♻️ extract movie display into MovieItem

--- src/front-end/App.tsx
+++ src/front-end/App.tsx
@@ -1,17 +1,28 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from "react"
+import type { Movie } from "../back-end/schemas/MoviesTypes"
+import MovieItem from "./components/MovieItem"
 
 export default function App() {
+  const [movies, setMovies] = useState<Movie[] | null>(null)
+
   useEffect(() => {
     fetch('/api/movies/popular')
       .then((response) => response.json())
       .then((data) => {
-        console.log(data)
+        setMovies(data.results)
       })
   }, [])
 
   return (
     <div>
-      Hello Vite from React!
+      <h1>Popular Movies</h1>
+      {movies ? (
+        <ul>
+          {movies.map((movie) => (
+            <MovieItem key={movie.id} movie={movie} />
+          ))}
+        </ul>
+      ) : null}
     </div>
   )
 }
